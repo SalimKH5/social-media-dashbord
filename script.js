f70d2a88126ad85ae538de678b2f244c5090220b
Salim khadir <63880607+SalimKH5@users.noreply.github.com>
2023-05-18 20:58:51 +0100

    changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button_mode=document.querySelector('.button-mode')
 const social_container=document.querySelector('.social-container')
 const btncontrol=document.querySelector('.button-control')
-
+const mode=document.getElementById('mode')
 const html=document.documentElement
 
 
@@ -10,15 +10,17 @@ button_mode.addEventListener('click',()=>{
     
     const currenttheme=html.getAttribute('data-theme')
     console.log(currenttheme)
-
    if(currenttheme === "light"){
     html.setAttribute('data-theme', 'dark');
     btncontrol.classList.remove('btn-left')
     btncontrol.classList.add('btn-right')
+    mode.innerHTML="Dark Mode"
 
    }else{
     html.setAttribute('data-theme', 'light');
     btncontrol.classList.remove('btn-right')
     btncontrol.classList.add('btn-left')
+    mode.innerHTML="Light Mode"
+    
    }
 })
